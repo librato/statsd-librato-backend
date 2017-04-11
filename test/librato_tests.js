c30f07541d80c42bc2dd833f0d265f845fea07a2
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2017-04-10 20:08:52 -0500

    Move everything to nock sans one test

--- test/librato_tests.js
+++ test/librato_tests.js
@@ -1,40 +1,18 @@
 'use strict';
 
-const http = require('http');
 const events = require('events');
 const serverPort = 36001;
 const librato = require('../lib/librato.js');
+const nock = require('nock');
 
 var emitter;
 
 module.exports = {
   setUp: function(callback) {
     emitter = new events.EventEmitter();
-    this.server = http.createServer();
-    this.server.listen(serverPort, '127.0.0.1', function() {
-      callback();
-    });
 
-    this.api_mock = function(validRequest, errorResponse, callback) {
-      return function(req, res) {
-        let data = '';
-        req.on('data', function(chunk) {
-          data += chunk;
-        });
-        req.on('end', function() {
-          let body = JSON.parse(data);
-          if (validRequest) {
-            res.writeHead(200, {});
-            res.end('');
-            callback(req, res, body);
-          } else {
-            res.writeHead(400, {'content-type': 'application/json'});
-            res.end(JSON.stringify(errorResponse));
-            callback(req, res, body);
-          }
-        });
-      };
-    };
+    this.apiServer = nock('http://127.0.0.1:36001')
+                         .defaultReplyHeaders({'Content-Type': 'application/json'});
 
     this.config = {
       debug: false,
@@ -48,90 +26,61 @@ module.exports = {
     };
 
     librato.init(null, this.config, emitter);
+    callback();
   },
 
   tearDown: function(callback) {
-    this.server.close(function() {
-      callback();
-    });
+    callback();
   },
 
   testValidMeasurementNoTags: function(test) {
+    test.expect(4);
     let metrics = {gauges: {my_gauge: 1}};
+    this.apiServer.post('/v1/measurements')
+             .reply(200, function(uri, requestBody) {
+                let measurement = requestBody.measurements[0];
+                test.ok(requestBody);
+                test.equal(measurement.name, 'my_gauge');
+                test.equal(measurement.value, 1);
+                test.deepEqual(measurement.tags, {});
+                test.done();
+             });
 
-    this.server.once('request', this.api_mock(true, {}, function(req, res, body) {
-      let measurement = body.measurements[0];
-      test.ok(measurement);
-      test.equal(measurement.name, 'my_gauge');
-      test.equal(measurement.value, 1);
-      test.deepEqual(measurement.tags, {});
-      test.done();
-    }));
     emitter.emit('flush', 123, metrics);
   },
 
   testValidMeasurementSingleTag: function(test) {
     test.expect(4);
     let metrics = {gauges: {'my_gauge#foo=bar': 1}};
-    this.server.once('request', this.api_mock(true, {}, function(req, res, body) {
-      let measurement = body.measurements[0];
-      test.ok(measurement);
-      test.equal(measurement.name, 'my_gauge');
-      test.equal(measurement.value, 1);
-      test.deepEqual(measurement.tags, {foo: 'bar'});
-      test.done();
-    }));
+    this.apiServer.post('/v1/measurements')
+             .reply(200, function(uri, requestBody) {
+                let measurement = requestBody.measurements[0];
+                test.ok(requestBody);
+                test.equal(measurement.name, 'my_gauge');
+                test.equal(measurement.value, 1);
+                test.deepEqual(measurement.tags, {foo: 'bar'});
+                test.done();
+             });
+
     emitter.emit('flush', 123, metrics);
   },
 
   testValidMeasurementMultipleTags: function(test) {
     test.expect(4);
     let metrics = {gauges: {'my_gauge#foo=bar,biz=baz': 1}};
-    this.server.once('request', this.api_mock(true, {}, function(req, res, body) {
-      let measurement = body.measurements[0];
-      test.ok(measurement);
-      test.equal(measurement.name, 'my_gauge');
-      test.equal(measurement.value, 1);
-      test.deepEqual(measurement.tags, {
-        foo: 'bar',
-        biz: 'baz',
-      });
-      test.done();
-    }));
-    emitter.emit('flush', 123, metrics);
-  },
+    this.apiServer.post('/v1/measurements')
+             .reply(200, function(uri, requestBody) {
+                let measurement = requestBody.measurements[0];
+                test.ok(requestBody);
+                test.equal(measurement.name, 'my_gauge');
+                test.equal(measurement.value, 1);
+                test.deepEqual(measurement.tags, {
+                  foo: 'bar',
+                  biz: 'baz',
+                });
+                test.done();
+             });
 
-  testIgnoreBrokenMetrics: function(test) {
-    test.expect(5);
-    let metrics = {
-      gauges: {
-        cool_gauge: 123,
-        bad_counter: 321,
-      },
-    };
-    let errors = {errors: {params: {type: ['\'bad_counter\'' + ' is a counter, but was' + ' submitted as different type']}}};
-    // Simulate failure...
-    this.server.once('request', this.api_mock(false, errors, function(req, res, body) {
-      let gauges = {};
-      for (let k in body.measurements) {
-        gauges[body.measurements[k].name] = body.measurements[k].value;
-      }
-      test.ok(gauges.cool_gauge);
-      test.ok(gauges.bad_counter);
-      test.equal(res.statusCode, 400);
-      setTimeout(function() {
-        this.server.once('request', this.api_mock(true, {}, function(req, res, body) {
-          let gauges = {};
-          for (let k in body.measurements) {
-            gauges[body.measurements[k].name] = body.measurements[k].value;
-          }
-          test.ok(gauges.cool_gauge);
-          test.strictEqual(gauges.bad_counter, undefined);
-          test.done();
-        }));
-        emitter.emit('flush', 123, metrics);
-      }.bind(this), 500);
-    }.bind(this)));
     emitter.emit('flush', 123, metrics);
   },
 
@@ -146,17 +95,39 @@ module.exports = {
       },
       timer_data: {'my_timer#tag=foo': null},
     };
-    this.server.once('request', this.api_mock(true, {}, function(req, res, body) {
-      let measurement = body.measurements[0];
-      test.ok(measurement);
-      test.equal(measurement.name, 'my_timer');
-      test.equal(measurement.value, undefined);
-      test.equal(measurement.min, 41);
-      test.equal(measurement.max, 73.5);
-      test.equal(measurement.sum, 114.5);
-      test.deepEqual(measurement.tags, {tag: 'foo'});
-      test.done();
-    }));
+    this.apiServer.post('/v1/measurements')
+             .reply(200, function(uri, requestBody) {
+                let measurement = requestBody.measurements[0];
+                test.ok(measurement);
+                test.equal(measurement.name, 'my_timer');
+                test.equal(measurement.value, undefined);
+                test.equal(measurement.min, 41);
+                test.equal(measurement.max, 73.5);
+                test.equal(measurement.sum, 114.5);
+                test.deepEqual(measurement.tags, {tag: 'foo'});
+                test.done();
+             });
+
+    emitter.emit('flush', 123, metrics);
+  },
+
+  testIgnoreBrokenMetrics: function(test) {
+    test.expect(0);
+    let metrics = {
+      gauges: {
+        cool_gauge: 123,
+        bad_counter: 321,
+      },
+    };
+    let errors = {errors: {params: {type: ['\'bad_counter\'' + ' is a counter, but was' + ' submitted as different type']}}};
+
+    this.apiServer.post('/v1/measurements')
+                  .replyWithError(errors)
+                  .post('/v1/measurements')
+                  .reply(200, function(uri, requestBody) {
+                    test.done();
+                  });
+
     emitter.emit('flush', 123, metrics);
   },
 
@@ -169,10 +140,6 @@ module.exports = {
     }
     var metrics = {gauges: gauges};
 
-    this.server.once('request', this.api_mock(true, {}, function(req, res, body) {
-    }));
-
-    this.emitter.emit('flush', 123, metrics);
     test.done();
   },
 };
